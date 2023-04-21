d5c816174826983a2dad53f6b8a7df004a1bfe09
r3tr0eth <jimenezgaizka13@protonmail.com>
2023-04-21 21:31:20 +0200

    fix - main style

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ def caesar_cipher(plaintext, shift):
     for char in plaintext:
         if char.isalpha():
             # Shift the character by the specified amount, wrapping around the alphabet if necessary
-            shifted_char = chr((ord(char) - 65 + shift) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + shift) % 26 + 97)
+            shifted_char = chr((ord(char) - 65 + shift) % 26 +
+                               65) if char.isupper() else chr((ord(char) - 97 + shift) % 26 + 97)
             ciphertext += shifted_char
         else:
             # Leave non-alphabetic characters unchanged
             ciphertext += char
     return ciphertext
+
 
 plaintext = "Hello, World!"
 shift = 2
